c0532eae9df7b6d0e92b06aa43c4987510075001
smail1983 <58273539+smail1983@users.noreply.github.com>
2026-09-21 16:30:27 +0100

    Remove old ad service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,2 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11846413
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
